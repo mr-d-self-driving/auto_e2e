f7a8d85b1856259b22fce5caa72ac90c9448cda1
Muhammad Zain Khawaja <m.zainkhawaja@gmail.com>
2026-05-25 17:03:00 +0300

    adding 2D conv to reduce channels and flatenning vector before linear layers

--- Model/model_components/driving_policy.py
+++ Model/model_components/driving_policy.py
@@ -1,13 +1,17 @@
+import torch
 import torch.nn as nn
 
 class DrivingPolicy(nn.Module):
     def __init__(self):
         super(DrivingPolicy, self).__init__()
 
+        # 2D Conv layer to reduce channels
+        self.reduce_channels = nn.Conv2d(1440, 24, 3, 1, 1)
+
         # Linear layers to process fused features
-        self.fc1 = nn.Linear(1440, 1440)
-        self.fc2 = nn.Linear(1440, 720)
-        self.fc3 = nn.Linear(720, 64)
+        self.fc1 = nn.Linear(1176, 1176)
+        self.fc2 = nn.Linear(1176, 588)
+        self.fc3 = nn.Linear(588, 64)
 
         # Dropout
         self.dropout = nn.Dropout(0.25)
@@ -17,8 +21,12 @@ class DrivingPolicy(nn.Module):
  
     def forward(self, fused_features):
 
+        # Reduce channels
+        feature_map = self.reduce_channels(fused_features)
+        feature_vector = torch.flatten(feature_map)
+
         # Multi-layer perceptron
-        f1 = self.fc1(fused_features)
+        f1 = self.fc1(feature_vector)
         f1 = self.activation(f1)
         f1 = self.dropout(f1)
 
