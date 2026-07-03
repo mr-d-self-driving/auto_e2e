93e275bd9e4ef9550f0e107f9220f87c880c50a0
riita10069 <ryota10069.tech5.jizi@gmail.com>
2026-07-03 19:50:18 +0900

    feat(nvidia): export make_map_tile
    Signed-off-by: riita10069 <ryota10069.tech5.jizi@gmail.com>

--- Model/data_parsing/nvidia_physical_ai/__init__.py
+++ Model/data_parsing/nvidia_physical_ai/__init__.py
@@ -1,10 +1,11 @@
-from .camera import CAMERA_NAMES, NUM_VIEWS, load_camera_frame
+from .camera import CAMERA_NAMES, NUM_VIEWS, load_camera_frame, make_map_tile
 from .dataset import NvidiaAVDataset
 from .egomotion import EGOMOTION_DIM, load_egomotion
- 
+
 __all__ = [
     "NvidiaAVDataset",
     "load_camera_frame",
+    "make_map_tile",
     "CAMERA_NAMES",
     "load_egomotion",
     "NUM_VIEWS",
