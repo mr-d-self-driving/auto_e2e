aba4597c428cb3a094ab7d3ec9bbeb3a8afe0575
riita10069 <ryota10069.tech5.jizi@gmail.com>
2026-07-17 11:48:02 +0900

    feat(training): export dataset policy for shared inference paths
    Signed-off-by: riita10069 <ryota10069.tech5.jizi@gmail.com>

--- Model/training/__init__.py
+++ Model/training/__init__.py
@@ -1,1 +1,33 @@
-"""Training entry points for AutoE2E."""
+"""Training entry points and dataset policies for AutoE2E."""
+
+from .dataset_policy import (
+    AUTO_E2E_TEMPORAL_DECAY,
+    AUTO_E2E_TIMESTEPS,
+    DatasetTrainingPolicy,
+    KITSCENES_TRAINING_POLICY,
+    LEGACY_KITSCENES_TRAINING_POLICY,
+    L2D_TRAINING_POLICY,
+    NVIDIA_TRAINING_POLICY,
+    adapt_egomotion_history,
+    group_uid_digest,
+    training_policy_from_config,
+    training_policy_for_dataset,
+    validation_group_uids,
+    validation_sample_identity,
+)
+
+__all__ = [
+    "AUTO_E2E_TEMPORAL_DECAY",
+    "AUTO_E2E_TIMESTEPS",
+    "DatasetTrainingPolicy",
+    "KITSCENES_TRAINING_POLICY",
+    "LEGACY_KITSCENES_TRAINING_POLICY",
+    "L2D_TRAINING_POLICY",
+    "NVIDIA_TRAINING_POLICY",
+    "adapt_egomotion_history",
+    "group_uid_digest",
+    "training_policy_from_config",
+    "training_policy_for_dataset",
+    "validation_group_uids",
+    "validation_sample_identity",
+]
